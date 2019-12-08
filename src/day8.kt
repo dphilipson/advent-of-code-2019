e3185b4c4ffad609da161c1da3c1e2f26ff706fc
David Philipson <david.philipson@gmail.com>
2019-12-07 23:37:53 -0800

    Add day 8

---
+++ src/day8.kt
@@ -0,0 +1,37 @@
+import util.readSingleString
+
+private const val WIDTH = 25
+private const val HEIGHT = 6
+private const val AREA = WIDTH * HEIGHT
+
+fun main() {
+    val input = readSingleString("src/day8.txt")
+    println(solvePart1(input))
+    solvePart2(input)
+}
+
+private fun solvePart1(input: String): Int =
+    input
+        .chunked(AREA)
+        .map { getCountsMap(it.asIterable()) }
+        .minBy { it.getValue('0') }!!
+        .let { it.getValue('1') * it.getValue('2') }
+
+private fun solvePart2(input: String) {
+    val layers = input.chunked(AREA)
+    val resolvedPixels = (0 until AREA)
+        .map { i -> resolveLayers( layers.indices.map { layers[it][i] }) }
+        .map { if (it == '0') ' ' else '#' }
+    resolvedPixels
+        .chunked(WIDTH)
+        .map { it.joinToString("") }
+        .forEach { println(it) }
+}
+
+private fun <T>getCountsMap(xs: Iterable<T>): Map<T, Int> {
+    val result = mutableMapOf<T, Int>().withDefault { 0 }
+    xs.forEach { result[it] = result.getValue(it) + 1 }
+    return result
+}
+
+private fun resolveLayers(layers: List<Char>): Char = layers.find { it != '2' }!!
