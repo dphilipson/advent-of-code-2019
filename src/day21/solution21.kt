03d5ca83b0ad414ee3da5c9c0e55cb94b7ac3150
David Philipson <david.philipson@gmail.com>
2019-12-22 12:07:33 -0800

    Add day 21

---
+++ src/day21/solution21.kt
@@ -0,0 +1,60 @@
+package day21
+
+import intcode.runIntcode
+import util.readLongs
+
+fun main() {
+    val input = readLongs("src/day21/input21.txt")[0]
+    println(solvePart2(input))
+}
+
+private fun solvePart1(input: List<Long>): Long {
+    // !(A && B && C) && D
+    val programText =
+        """
+            NOT J J
+            AND A J
+            AND B J
+            AND C J
+            NOT J J
+            AND D J
+            WALK
+            
+        """.trimIndent()
+    val program = programText.asSequence()
+        .map { it.toLong() }
+        .toList()
+    return runIntcode(input, program).outputs.last()
+}
+
+private fun solvePart2(input: List<Long>): Long {
+    // Previous condition, and also
+    // !(!E && !H) -> E || H
+    val programText =
+        """
+            NOT J J
+            AND A J
+            AND B J
+            AND C J
+            NOT J J
+            AND D J
+            OR E T
+            OR H T
+            AND T J
+            RUN
+            
+        """.trimIndent()
+    val program = programText.asSequence()
+        .map { it.toLong() }
+        .toList()
+    printOutput(runIntcode(input, program).outputs)
+    return runIntcode(input, program).outputs.last()
+}
+
+private fun printOutput(output: List<Long>) {
+    output.asSequence()
+        .map { it.toChar() }
+        .joinToString("")
+        .splitToSequence("\n")
+        .forEach { println(it) }
+}
